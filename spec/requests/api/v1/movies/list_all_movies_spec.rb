cff3973e6515f02b1f93837e90bd99d6e0b86458
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 16:07:10 -0700

    Update spec

--- spec/requests/api/v1/movies/list_all_movies_spec.rb
+++ spec/requests/api/v1/movies/list_all_movies_spec.rb
@@ -17,8 +17,17 @@ RSpec.describe 'List all movies' do
       movies_json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
-      require 'pry'; binding.pry
       expect(movies_json[:data].size).to eq(50)
+
+      expect(movies_json[:data][0][:type]).to eq('movie')
+      expect(movies_json[:data][0][:attributes]).to have_key(:imbd_id)
+      expect(movies_json[:data][0][:attributes]).to have_key(:title)
+      expect(movies_json[:data][0][:attributes]).to have_key(:genres)
+      expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
+      expect(movies_json[:data][0][:attributes]).to have_key(:budget)
+      expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
+
+      expect(movies_json[:data][0][:attributes][:budget].include?('$')).to eq(true)
     end
   end
 end
