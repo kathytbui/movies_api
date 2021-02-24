073a34125e2285546a18f364163ae6203dd14a17
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 16:06:18 -0700

    Create spec for list all movies feature

--- spec/requests/api/v1/movies/list_all_movies_spec.rb
+++ spec/requests/api/v1/movies/list_all_movies_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
+# * An endpoint exists that lists all movies
+# * List is paginated: 50 movies per page, the page can be altered with the `page` query params
+# * Columns should include: imdb id, title, genres, release date, budget
+# * Budget is displayed in dollars
+
 RSpec.describe 'List all movies' do
   describe 'As a user' do
     before :each do
@@ -7,10 +12,12 @@ RSpec.describe 'List all movies' do
     end
 
     it 'can get a list of all movies' do
-      get '/api/v1/movies'
+      params = { page: 1 }
+      get '/api/v1/movies', params: params
       movies_json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
+      require 'pry'; binding.pry
       expect(movies_json[:data].size).to eq(50)
     end
   end
