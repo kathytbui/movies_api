3d223c2d927690a43c1a5020cd90c14f503ffe76
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 19:49:33 -0700

    Update release date type to datetime

--- db/migrate/20210224211618_create_movies.rb
+++ db/migrate/20210224211618_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :overview
       t.string :production_companies
-      t.string :release_date
+      t.datetime :release_date
       t.integer :budget
       t.integer :revenue
       t.integer :runtime
