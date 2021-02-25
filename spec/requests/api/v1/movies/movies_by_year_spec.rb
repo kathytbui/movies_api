ddcb61dcb93cfa7c6505d397e74b22e1ff95806d
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 19:50:04 -0700

    Add tests for movies by year feature. Change .size to .count

--- spec/requests/api/v1/movies/movies_by_year_spec.rb
+++ spec/requests/api/v1/movies/movies_by_year_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
-# * An endpoint exists that lists all movies
+# * An endpoint exists that will list all movies from a particular year
 # * List is paginated: 50 movies per page, the page can be altered with the `page` query params
-# * Columns should include: imdb id, title, genres, release date, budget
-# * Budget is displayed in dollars
+# * List is sorted by date in chronological order
+# * Sort order can be descending
+# * Columns include: imdb id, title, genres, release date, budget
 
-RSpec.describe 'List all movies' do
+RSpec.describe 'Movie by year' do
   describe 'As a user' do
     before :each do
-      @movies = create_list(:movie, 60)
+      50.times { create(:movie, release_date: Time.now.utc + 1.days) }
+      @movie2 = create(:movie, release_date: Time.now.utc - 30 .days)
+      @movie3 = create(:movie, release_date: Time.now.utc - 20.days)
     end
 
-    it 'can get a list of all movies' do
-      params = { page: 1 }
-      get '/api/v1/movies', params: params
+    it "text" do
+      get "/api/v1/movies/find_all?release_date=2021?page=1"
       movies_json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
       expect(movies_json[:data].count).to eq(50)
+
+      first_movie_date = movies_json[:data][0][:attributes][:release_date]
+      second_movie_date = movies_json[:data][1][:attributes][:release_date]
+      greater_date = second_movie_date > first_movie_date
+
+      expect(greater_date).to eq(true)
 
       expect(movies_json[:data][0][:type]).to eq('movie')
       expect(movies_json[:data][0][:attributes]).to have_key(:imdb_id)
