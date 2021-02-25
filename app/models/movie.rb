ea6a30cbbf2684e69bf5ce43ac733b95a400bd0f
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 19:51:31 -0700

    Create module to handle searching for movies based on a param

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 class Movie < ApplicationRecord
+  include Filterable
   has_many :ratings
+
+  scope :filter_by_release_date, ->(release_date) { where('extract(year from release_date) = ?', release_date.to_i).order(:release_date) }
 
   def average_rating
     sum = self.ratings.sum do |rating|
