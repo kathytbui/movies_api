0d83fd4d0bad215284c97017e1354a4ceb65f991
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-24 17:30:09 -0700

    Add show path for movies

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::MoviesController < ApplicationController
     movies = Movie.limit(50).offset(params[:page])
     render json: MovieSerializer.new(movies)
   end
+
+  def show
+    render json: MovieDetailsSerializer.new(Movie.find(params[:id]))
+  end
 end
