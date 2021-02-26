c5daf69a03591877559994826e3130e132ae24ef
Kathy Bui <kathy.bui1992@gmail.com>
2021-02-26 08:12:44 -0700

    Uncomment code

--- spec/requests/api/v1/movies/movies_by_year_spec.rb
+++ spec/requests/api/v1/movies/movies_by_year_spec.rb
@@ -8,37 +8,37 @@ require 'rails_helper'
 
 RSpec.describe 'Movie by year' do
   describe 'As a user' do
-    # it "can get a list of movies by year in ascending order by default" do
-    #   50.times { create(:movie, release_date: Time.now.utc + 1.days) }
-    #   create(:movie, release_date: Time.now.utc - 30.days)
-    #   create(:movie, release_date: Time.now.utc - 20.days)
-    #
-    #   params = {
-    #     "release_date" => 2021,
-    #     "page" => 1
-    #   }
-    #   get "/api/v1/movies/find_all"
-    #   movies_json = JSON.parse(response.body, symbolize_names: true)
-    #
-    #   expect(response).to be_successful
-    #   expect(movies_json[:data].count).to eq(50)
-    #
-    #   first_movie_date = movies_json[:data][0][:attributes][:release_date]
-    #   second_movie_date = movies_json[:data][1][:attributes][:release_date]
-    #   greater_date = second_movie_date > first_movie_date
-    #
-    #   expect(greater_date).to eq(true)
-    #
-    #   expect(movies_json[:data][0][:type]).to eq('movie')
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:imdb_id)
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:title)
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:genres)
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:budget)
-    #   expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
-    #
-    #   expect(movies_json[:data][0][:attributes][:budget].include?('$')).to eq(true)
-    # end
+    it "can get a list of movies by year in ascending order by default" do
+      50.times { create(:movie, release_date: Time.now.utc + 1.days) }
+      create(:movie, release_date: Time.now.utc - 30.days)
+      create(:movie, release_date: Time.now.utc - 20.days)
+
+      params = {
+        "release_date" => 2021,
+        "page" => 1
+      }
+      get "/api/v1/movies/find_all"
+      movies_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(movies_json[:data].count).to eq(50)
+
+      first_movie_date = movies_json[:data][0][:attributes][:release_date]
+      second_movie_date = movies_json[:data][1][:attributes][:release_date]
+      greater_date = second_movie_date > first_movie_date
+
+      expect(greater_date).to eq(true)
+
+      expect(movies_json[:data][0][:type]).to eq('movie')
+      expect(movies_json[:data][0][:attributes]).to have_key(:imdb_id)
+      expect(movies_json[:data][0][:attributes]).to have_key(:title)
+      expect(movies_json[:data][0][:attributes]).to have_key(:genres)
+      expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
+      expect(movies_json[:data][0][:attributes]).to have_key(:budget)
+      expect(movies_json[:data][0][:attributes]).to have_key(:release_date)
+
+      expect(movies_json[:data][0][:attributes][:budget].include?('$')).to eq(true)
+    end
 
     it "can get a list of movies in descending order" do
       50.times { create(:movie, release_date: Time.now.utc - 30.days) }
